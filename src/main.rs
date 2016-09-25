67dd752669bfce2f67e79e28f9f4e0b714951e5d
Mateusz Dobek <mat.dobek@gmail.com>
2016-09-25 13:12:55 +0200

    Make simple tcp server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::net::{TcpListener, TcpStream};
+use std::{thread, time};
+
+fn handle_client(stream: TcpStream) {
+    println!("Connection Success!");
+}
+
 fn main() {
-    println!("Sopot server!");
+    println!("Starting Sopot server...");
+
+    let listener = TcpListener::bind("127.0.0.1:3000").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move || {
+                    handle_client(stream);
+                });
+            },
+            Err(e) => {
+                println!("Connection failed!");
+            },
+        }
+    }
+
+    drop(listener);
 }
