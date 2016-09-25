1fbd9c4eae92469d33511fe89c0c480816f03367
Mateusz Dobek <mat.dobek@gmail.com>
2016-09-25 21:29:49 +0200

    Obtain peer port and ip

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
-use std::net::{TcpListener, TcpStream};
+use std::net::{TcpListener, TcpStream, SocketAddr};
 use std::{thread, time};
 
 fn handle_client(stream: TcpStream) {
-    println!("Connection Success!");
+    //stream.local_addr()
+    match stream.peer_addr() {
+        Ok(socket_addr) => {
+            println!("Peer ip: {}, port: {}", socket_addr.ip(), socket_addr.port());
+        },
+        Err(e) => {
+            println!("Obtaining address failed!");
+        },
+    }
 }
 
 fn main() {
